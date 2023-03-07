d3e13e24e6ff5026076c051177d5bfd90bb425db
Boniwayne <Boniwayne125@gmail.com>
2023-03-07 13:00:55 -0800

    Task 0

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10,10)
+number = range(-10,10)
+for i in number
+if i < 1:
+    print("i is negative")
+elif i = 0:
+    print(i is zero)
+elif i > 1:
+    print(i is positive)
+
